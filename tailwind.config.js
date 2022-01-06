fff66655b67562d03d267b8389d271b807231c97
Jorge Arturo Sarabia <jsarabiar98@gmail.com>
2022-01-06 14:24:32 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      "inconsolata": ["Inconsolata", "monospace"],
+      "space-mono": ["Space Mono", "monospace"],
+      "montserrat": ["Montserrat", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
